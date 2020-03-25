d5db9b832e23d1e3a7ced3cfbcc770092cae926c
Maximilian Köstler <maximilian@koestler.hamburg>
2020-03-25 22:23:19 +0100

    Install python language server

--- 04_build_ghdl_ls.ps1
+++ 04_build_ghdl_ls.ps1
@@ -4,21 +4,31 @@ Function Install-MingwPath {
     $Env:Path += ";$MingwBase\msys\1.0\bin"
 }
 
-Function Invoke-Pip {
-    # ...
+Function Install-GhdlPath {
+    $GhdlBase = "$(Get-Location)\build\ghdl_artifacts"
+    $Env:Path += ";$GhdlBase\bin"
+    $Env:Path += ";$GhdlBase\lib"
+}
+
+Function Invoke-Pip($PythonPath) {
+    & "$PythonPath\pip" install .
 }
 
 Function Main() {
     $BaseDir = Get-Location
-    $OriginalPath = $Env:Path
+    Push-Location "build\ghdl\python"
+    Invoke-Pip("$BaseDir\build\python\Scripts\")
+    Pop-Location
 
+    $OriginalPath = $Env:Path
     $Env:Path = ""
+
     Install-MingwPath
+    Install-GhdlPath
     Write-Host $Env:Path
 
-    Push-Location "build\ghdl"
-    Invoke-Pip
-    Pop-Location
+    # Test that we can run gldl-ls
+    & "build\python\Scripts\ghdl-ls.exe" --disp-config
 
     $Env:Path = $OriginalPath
 }
